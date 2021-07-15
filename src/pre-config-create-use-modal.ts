4c6c91e231c409e697c867b1604e1b87d5c236fe
Jimmy Lee <jimmy@soomgo.com>
2021-07-15 16:12:59 +0900

    fix: remove unnecessary arguments from preConfigCreateUseModal

--- src/pre-config-create-use-modal.ts
+++ src/pre-config-create-use-modal.ts
@@ -8,10 +8,7 @@ import _ from 'lodash';
 /**
  * createUseModal creation function with default value of option argument set
  */
-export const preConfigCreateUseModal = (
-  _createUseModal: typeof createUseModal,
-  _option: CreateModalOption
-) => {
+export const preConfigCreateUseModal = (_option: CreateModalOption) => {
   const composed = <Data extends unknown = void, Param extends unknown = void>(
     ...param: CreateModalFunctionParam<Data, Param>
   ) => {
